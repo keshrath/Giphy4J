80f2ac531fd27826d94f1d7ca85508ce6173aed2
Mathias Markl <mathias@mukprojects.at>
2016-03-19 18:02:00 +0100

    serach test

--- src/main/java/at/mukprojects/giphy4j/Giphy.java
+++ src/main/java/at/mukprojects/giphy4j/Giphy.java
@@ -1,5 +1,91 @@
+/** 
+ * This code is copyright (c) Mathias Markl 2016
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package at.mukprojects.giphy4j;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.commons.io.IOUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import at.mukprojects.giphy4j.models.SearchFeed;
+
+/**
+ * This class represents the main API class.
+ *
+ * @author Mathias Markl
+ */
 public class Giphy {
+
+    private static final String searchQuery = "http://api.giphy.com/v1/gifs/search?";
+
+    private String apiKey;
+    private Gson gson;
+
+    public Giphy(String apiKey) {
+	this.apiKey = apiKey;
+	gson = new GsonBuilder().create();
+    }
+
+    public SearchFeed search(String query, int offset) throws IOException {
+	return search(query, 25, offset);
+    }
+
+    public SearchFeed search(String query, int limit, int offset) throws IOException {
+	SearchFeed feed = null;
+
+	if (limit > 100) {
+	    limit = 100;
+	}
+
+	String formattedQuery = query.replace(" ", "+");
+	String queryString = searchQuery;
+
+	queryString += "q=" + formattedQuery;
+	queryString += "&";
+	queryString += "limit=" + limit;
+	queryString += "&";
+	queryString += "offset=" + offset;
+	queryString += "&";
+	queryString += "api_key=" + apiKey;
+
+	URL url = new URL(queryString);
+	URLConnection con = url.openConnection();
+	InputStream in = con.getInputStream();
+
+	String encoding = con.getContentEncoding();
+	encoding = encoding == null ? "UTF-8" : encoding;
+
+	String body = IOUtils.toString(in, encoding);
+
+	try {
+	    feed = gson.fromJson(body, SearchFeed.class);
+	} catch (Exception e) {
+	    e.printStackTrace(); // TODO
+	} finally {
+	    in.close();
+	}
+
+	return feed;
+    }
 
 }
--- src/test/java/at/mukprojects/giphy4j/models/SerachFeedTest.java
+++ src/test/java/at/mukprojects/giphy4j/models/SerachFeedTest.java
@@ -1,9 +1,27 @@
+/** 
+ * This code is copyright (c) Mathias Markl 2016
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package at.mukprojects.giphy4j.models;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.StringWriter;
 
 import org.apache.commons.io.IOUtils;
@@ -14,28 +32,38 @@ import org.junit.Test;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import at.mukprojects.giphy4j.Giphy;
+
+/**
+ * SerachFeed test class.
+ *
+ * @author Mathias Markl
+ */
 public class SerachFeedTest {
 
+    private final static String API_KEY = "dc6zaTOxFJmzC";
     private final static String RESFILE = "src/test/resources/search-response.json";
 
     private StringWriter writer;
     private FileInputStream inputStream;
-    
+
     private Gson gson;
     private String jsonResponse;
 
+    private Giphy giphy;
 
     @Before
     public void setUp() throws Exception {
 	File inputFile = new File(RESFILE);
-	
+
 	inputStream = new FileInputStream(inputFile);
 	writer = new StringWriter();
-	
+
 	IOUtils.copy(inputStream, writer, "UTF-8");
 	jsonResponse = writer.toString();
-	
+
 	gson = new GsonBuilder().create();
+	giphy = new Giphy(API_KEY);
     }
 
     @After
@@ -44,10 +72,25 @@ public class SerachFeedTest {
 	inputStream.close();
     }
 
+    /**
+     * Tests the construction of the SearchFeed model by parsing a sample API
+     * response. The sample Json String is taken from the search-response file.
+     */
     @Test
-    public void testSearchFeed() {
+    public void testSearchFeedModel() {
 	SearchFeed searchFeed = gson.fromJson(jsonResponse, SearchFeed.class);
-	System.out.println(searchFeed);
+
+	assertTrue(searchFeed.getMeta().getStatus() == 200);
+	assertTrue(searchFeed.getPagination().getCount() == 2);
+	assertTrue(searchFeed.getDataList().size() >= 1);
+	assertEquals(searchFeed.getDataList().get(0).getId(), "QgcQLZa6glP2w");
+	assertEquals(searchFeed.getDataList().get(0).getImages().getOriginal().getUrl(),
+		"http://media2.giphy.com/media/QgcQLZa6glP2w/giphy.gif");
+
     }
 
+    @Test
+    public void testSearchFeedRequest() throws IOException {
+	assertTrue(giphy.search("cat", 1, 0) != null);
+    }
 }
